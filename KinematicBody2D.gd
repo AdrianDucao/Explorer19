3ac0730be6c2fe96f035342751a203b0469c2e30
AdrianDucao <adrianducao@gmail.com>
2020-03-25 10:15:24 +0800

    major update

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,7 +6,7 @@ var motion = Vector2()
 var speed = 300
 var gravity = 15
 var jump = -300
-var on_ground = false
+var on_ground = true
 
 func _physics_process(delta):
 	motion.y += gravity
@@ -18,18 +18,20 @@ func _physics_process(delta):
 		motion.x = -speed
 		$AnimatedSprite.play("walk-left")
 		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("ui_up"):
-		if on_ground == true:
-			motion.y = jump
-			on_ground = false
-	else:
+	elif Input.is_action_just_released("ui_left"):
+		motion.x = 0
+		$AnimatedSprite.play("idle2")
+	elif Input.is_action_just_released("ui_right"):
 		motion.x = 0
 		$AnimatedSprite.play("idle1")
-		if on_ground == true:
-			$AnimatedSprite.play("idle1")
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = jump
+			on_ground = false
+			#if on_ground == false:
+			#	$AnimatedSprite.play("fall-right")
+			#else:
+			#	$AnimatedSprite.play("idle1")
 	
 	move_and_slide(motion, UP)
